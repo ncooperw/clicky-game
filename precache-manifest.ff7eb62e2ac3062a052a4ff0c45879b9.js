2ef6842fa4e3451dec13b6a473dbf7a18f1c7265
Nicole Cooper-White <nwhite1018@gmail.com>
2018-10-06 13:01:26 -0600

    Updates

---
+++ precache-manifest.ff7eb62e2ac3062a052a4ff0c45879b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c34e47386b5b3d495f3d25f7e8aa63",
+    "url": "/clicky-game/static/media/Galaxy.93c34e47.jpg"
+  },
+  {
+    "revision": "fd6e27e45ea8fef8e392",
+    "url": "/clicky-game/static/css/main.806dcc2b.chunk.css"
+  },
+  {
+    "revision": "057d018cd02752b3bfb0",
+    "url": "/clicky-game/static/js/1.057d018c.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "57669b84b7e2ef8af4682cf549bc3ea7",
+    "url": "/clicky-game/static/media/61Oslb-0BYL._SX425_.57669b84.jpg"
+  },
+  {
+    "revision": "a3d50ea44f83476648229497185a451c",
+    "url": "/clicky-game/static/media/BlackAndWhite.a3d50ea4.jpg"
+  },
+  {
+    "revision": "041cff4d331cc1eca3990fcc951358d9",
+    "url": "/clicky-game/static/media/Blue.041cff4d.jpg"
+  },
+  {
+    "revision": "fd6e27e45ea8fef8e392",
+    "url": "/clicky-game/static/js/main.fd6e27e4.chunk.js"
+  },
+  {
+    "revision": "46746a73d874f75a1c0e51b0e077fc89",
+    "url": "/clicky-game/static/media/GrayAndBlack.46746a73.jpg"
+  },
+  {
+    "revision": "dc52bc1c31b3b83e8795823202abbe50",
+    "url": "/clicky-game/static/media/RedWhiteAndBlack.dc52bc1c.jpg"
+  },
+  {
+    "revision": "659752f1c2e3758aa98f63b001f6673b",
+    "url": "/clicky-game/static/media/WhiteAndBlue.659752f1.jpg"
+  },
+  {
+    "revision": "17c2e55f04bfb1a2c40cbccc9fa6be8a",
+    "url": "/clicky-game/static/media/WhiteAndRed.17c2e55f.jpg"
+  },
+  {
+    "revision": "31a9e1ace8eae0cae59a9cf1d7b99647",
+    "url": "/clicky-game/static/media/YellowAndGray.31a9e1ac.jpg"
+  },
+  {
+    "revision": "ddbe0f32b2ddc455e712f5baabebd797",
+    "url": "/clicky-game/index.html"
+  }
+];
